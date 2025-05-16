c8e00b0591535648305afc362aaacc093e97f577
CringeCore92 <norenko-dan@yandex.ru>
2025-05-16 21:52:04 +0300

    дипломка тест

---
+++ mobile phones and gadgets/js/main.js
@@ -0,0 +1,12 @@
+
+const swiper = new Swiper('.mySwiper', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    slidesPerView: 1, // О
+    speed: 500, // С
+});
+
